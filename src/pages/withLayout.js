5f682fc9e67a1838057c505bd1687b4646979530
Suglo Suleman Rufiatu <101270194+rufia-suleman@users.noreply.github.com>
2022-04-01 05:34:09 -0700

    navigate

--- src/pages/withLayout.js
+++ src/pages/withLayout.js
@@ -11,8 +11,8 @@ function Layout(){
         <div>
         <nav>
                 <span className='nav-link'><Link to="/">Home</Link></span>
-                <span className='nav-link'><Link to="/">About</Link></span>
-                <span className='nav-link'><Link to="/">Blog</Link></span>
+                <span className='nav-link'><Link to="/about">About</Link></span>
+                <span className='nav-link'><Link to="/blog">Blog</Link></span>
         </nav>
         </div>
 
