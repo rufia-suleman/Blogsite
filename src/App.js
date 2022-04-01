0591790c8ad833c229c919b7d4ce6149e70809d1
Suglo Suleman Rufiatu <101270194+rufia-suleman@users.noreply.github.com>
2022-04-01 09:15:26 -0700

    added some items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import Home from './pages/Home'
-import About from './pages/About'
-import Blog from './pages/blog/Blog'
-import {Routes, Route, Navigate} from "react-router-dom"
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import {Routes, Route, Navigate} from "react-router-dom";
+import BlogList from './pages/Blog/blogs';
+import BlogDetails from './pages/Blog/detail';
 
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/about" element={<About/>} />
-    <Route path="/blog" element={<Blog/>} />
+    <Route path="/blog" element={<Blog/>}>
+    <Route index element={<BlogList/>}/>
+    <Route path=":blog" element={<BlogDetails/>}/>
+    </Route>
     <Route path='*' element={<Navigate to="/"/>} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/withLayout.js
+++ src/pages/withLayout.js
@@ -1,8 +1,26 @@
-import React, { Component } from 'react'
-import {Link} from "react-router-dom"
+import React, {useEffect, useState } from 'react'
+import {Link, useLocation} from "react-router-dom"
 
 function withLayout(Component) {
 function Layout(){
+  const location = useLocation();
+  const [activeNav, setActiveNav]= useState("home")
+
+  useEffect(()=>{
+    let currentLoc = location.pathname.split("/")
+    let current = currentLoc[1];
+    switch(current) {
+      case "about":
+        setActiveNav("about");
+        break;
+        case "blog":
+        setActiveNav("blog");
+        break;
+        default:
+        setActiveNav("home");
+        break;
+    }
+  }, [location.pathname]);
 
     return(
         <div className='layout'>
@@ -10,9 +28,9 @@ function Layout(){
         <span>Logo</span>
         <div>
         <nav>
-                <span className='nav-link'><Link to="/">Home</Link></span>
-                <span className='nav-link'><Link to="/about">About</Link></span>
-                <span className='nav-link'><Link to="/blog">Blog</Link></span>
+                <span className={`nav-link ${activeNav === "home" ? "active" : ""}`}><Link to="/">Home</Link></span>
+                <span className={`nav-link ${activeNav === "about" ? "active" : ""}`}><Link to="/about">About</Link></span>
+                <span className={`nav-link ${activeNav === "blogs" ? "active" : ""}`}><Link to="/blog">Blog</Link></span>
         </nav>
         </div>
 
